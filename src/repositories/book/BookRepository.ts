a23f59d5b94268ad5d1eb7bacab582bdd9bf1f7b
torish14 <nanamin.104@icloud.com>
2022-01-13 06:17:59 +0900

    ✨ feat: types.ts の型定義を読み込んで axios のインスタンスを使用する

---
+++ src/repositories/book/BookRepository.ts
@@ -0,0 +1,13 @@
+import type { BookItem, BookRepositoryInterface, Params, Result } from "./types"
+import { httpClient } from '../httpClient'
+
+export class BookRepository implements BookRepositoryInterface {
+	async get(params: Params): Promise<Result> {
+		const { data } = await httpClient.get<Result>('/', { params })
+		return data
+	}
+	async find(id: string): Promise<BookItem> {
+		const { data } = await httpClient.get<BookItem>(`/${id}`)
+		return data
+	}
+}
